bf7e440922f7f6475593d8afbb42a1f5d6d6088a
Fabiantonio <fa.casas@duocuc.cl>
2024-11-22 02:09:02 -0300

    add bootstrap4

--- LittleShop/settings.py
+++ LittleShop/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Directorio donde Django almacenará los archivos estáticos para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
